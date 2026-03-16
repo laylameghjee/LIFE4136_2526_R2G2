e8ab03d79b262ae295e34762a1a25384aa395242
laylameghjee <mbxlm9@nottingham.ac.uk>
2026-03-16 12:55:55 +0000

    star indexing

---
+++ 6_StarIndexing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=STAR_index
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=90g
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=XXX@nottingham.ac.uk
+#SBATCH --mail-type=begin
+#SBATCH --mail-type=fail
+#SBATCH --mail-type=end
+
+source $HOME/.bash_profile #Allows conda use
+conda activate tbrucei #Activates rotation2 env
+
+STAR --runMode genomeGenerate \
+        --genomeDir XXX/ReferenceGenomes \
+        --genomeFastaFiles XXX/ReferenceGenomes/GRCh38.fa \
+        --sjdbGTFfile XXX/ReferenceGenomes/GRCH38.gtf \
+        --sjdbOverhang 99 \
+        --runThreadN 8 \
+#Runs STAR to index samples. Sets overhang lenght as 99 bp, and uses 8 cores
